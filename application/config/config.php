2f70063143fdf050bbbb2cfe5581601133d02647
jogendra-yadav <jogendra_yadav@ymail.com>
2017-05-29 18:50:54 +0530

    Change timezone

--- application/config/config.php
+++ application/config/config.php
@@ -25,6 +25,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   |
  */
 
+date_default_timezone_set('Asia/Kolkata');
+
 if (isset($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == "localhost") {
     $config['base_url'] = 'http://localhost/water-supply';
 }else{
