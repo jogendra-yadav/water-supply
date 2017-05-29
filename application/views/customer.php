ae6cab16c487ac482d5a160a52b6943cbdbf3801
jogendra-yadav <jogendra_yadav@ymail.com>
2017-05-29 19:44:32 +0530

    Change Login user type

--- application/views/customer.php
+++ application/views/customer.php
@@ -32,7 +32,7 @@ $this->load->view('navbar');
                                         <td><?php echo $customer->first_name . " " . $customer->last_name; ?></td>
                                         <td class="center"><?php echo $customer->address; ?></td>
                                         <td class="center"><?php echo $customer->mobile; ?></td>
-                                        <td class="center"><?php echo date('d-F-Y', strtotime($customer->register_date)); ?></td>
+                                        <td class="center"><?php echo date('d-F-Y @ h:i A', strtotime($customer->register_date)); ?></td>
                                         <td class="center">
                                             <a href="<?php echo site_url('edit_customer_' . $customer->id_customer . '.html'); ?>"><button class="btn btn-primary btn-xs" type="button"><i class="fa fa-pencil"></i></button></a>
                                             <button class="btn btn-danger btn-xs delete_customer_btn" path="<?php echo site_url('delete_customer_' . $customer->id_customer . '.html'); ?>" type="button"><i class="fa fa-trash-o"></i></button>                                        
